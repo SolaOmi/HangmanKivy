a0f090c08c64059bed031db3e39ce2cfbd89187f
sola <solaocodes@gmail.com>
2017-06-07 22:06:21 -0400

    game works, needs animation.

--- main.py
+++ main.py
@@ -1,83 +1,79 @@
 #!/usr/bin/env python3
 
-from kivy.app import App
-from kivy.clock import Clock
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
+# Hangman
+# By Bisola Bruno solaocodes@gmail.com
+
+import os
+import sys
 from random import randint
 
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.config import Config
+from kivy.uix.boxlayout import BoxLayout
+
+FPS = 1 / 60
+WINDOWWIDTH = 500
+WINDOWHEIGHT = 600
+RESIZABLE = 0
+NUM_OF_GUESSES = 5
 
 class HangmanBoard(BoxLayout):
-    words = {"Random": ["food", "boy", "animal", "zebra"], "Numbers": ["one", "two", "three"]}
-    categories = list(words.keys())
+
+    files = list(filter(lambda x: x.endswith('.txt'), os.listdir()))
+    file_in = files[randint(0,len(files)-1)]
+    with open(file_in, 'r') as f:
+        words = f. readlines()
+
+    words = [i.strip() for i in words]
+    category = words[0]
+    full_word = words[randint(1, len(words)-1)]
+    empty_word = ['_' if i.isalpha() else i for i in full_word]
     misses = []
-    guesses = []
-    category = categories[randint(0, len(categories)-1)]
-    word = words[category][randint(0, len(words[category])-1)]
-    empty = ["_"]*len(word)
-    nguess = 0
 
-    # widgets
+    def letter_click(self, letter):
 
-    #category_label = self.ids['label1']
-    #empty_word_label = self.ids['label2A']
-    #empty_word = self.ids['label2B']
-    #misses_label = self.ids['label2D']
-    #misses = self.ids['label2E']
-    #last_guess_label = self.ids['label2F']
-    #last_guess = self.ids['label2G']
-    #illustration = self.ids['label3']
-    #letter_input = self.ids['label4']
+        letter.disabled = True
+        letter.disabled_color = [1,1,1,1]
+        letter.background_disabled_normal = ''
 
-    def label_names(self, *args):
-        category_label = self.ids['label1']
-        empty_word_label = self.ids['label2A']
-        misses_label = self.ids['label2D']
-        last_guess = self.ids['label2F']
+        guess = letter.text.lower()
+        answer = self.full_word.lower()
 
-        category_label.text = self.category
-        empty_word_label.text = 'WORD'
-        misses_label.text = 'MISSES'
-        last_guess.text = 'LAST GUESS'
+        letter.background_color = [1,0,0,0.5]
+        if guess in answer:
+            for i in range(len(answer)):
+                if guess == answer[i]:
+                    self.empty_word[i] = self.full_word[i]
+                    letter.background_color = [0,1,0,0.5]
+        else:
+            self.misses.append(guess)
 
     def update(self, *args):
-        empty_word = self.ids['label2B']
-        misses = self.ids['label2E']
-        #last_guess = self.ids['label2G']
-        #illustration = self.ids['label3']
-        letter_input = self.ids['label4']
 
-        empty_word.text = ' '.join(self.empty)
-        misses.text =  ' '.join(self.misses)
+        self.ids['label1'].text = self.category
+        self.ids['label3'].text = ' '.join(self.empty_word)
 
-        if self.nguess < 5:
+        if self.full_word == ''.join(self.empty_word):
+            self.ids['label2'].text = 'YOU WIN!'
+            Clock.schedule_once(self.terminate, 5)
+        elif NUM_OF_GUESSES == len(self.misses):
+            self.ids['label2'].text = 'YOU LOSE!'
+            Clock.schedule_once(self.terminate, 5)
 
-            guess = letter_input.text
+    def terminate(self, *args):
+        exit()
 
-            if guess in self.word:
-                for i in range(len(self.word)):
-                    if guess == self.word[i]:
-                        self.empty[i] = guess
-            else:
-                self.misses.append(guess)
-                self.nguess += 1
-
-            if self.word == "".join(self.empty):
-                #print("\nYou win!")
-                exit(0)
-
-        #if self.nguess >= 5:
-            #exit(0)
 
 class HangmanApp(App):
 
+    Config.set('graphics', 'resizable', RESIZABLE)
+    Config.set('graphics', 'width', WINDOWWIDTH)
+    Config.set('graphics', 'height', WINDOWHEIGHT)
+
     def build(self):
         game = HangmanBoard()
-        game.label_names()
-        Clock.schedule_interval(game.update, 1/60)
+        Clock.schedule_interval(game.update, FPS)
         return game
 
 
